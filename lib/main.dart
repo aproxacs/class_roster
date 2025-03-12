d1b21107580109c32dc7651f32cefddb511eb87a
aproxacs <aproxacs@gmail.com>
2025-03-12 13:20:10 +0900

    시스템 다크모드 설정 지원 추가

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,20 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: '출석부',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.light,
+        ),
         useMaterial3: true,
       ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.system,
       home: const HomeScreen(),
     );
   }
